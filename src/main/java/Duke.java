ef247366f0b5457d059d82eb6e71c6fcdfbe99cb
killingbear999 <e0424629@u.nus.edu>
2020-08-23 22:59:33 +0800

    adding echos messages and exit requirment

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        start();
+
+        String userCommand;
+        String exitKeyword = "bye";
+        boolean checkExitKeyword = false;
+        Scanner userInput = new Scanner(System.in);
+        userCommand = userInput.nextLine();
+
+        do {
+            if (userCommand.equals(exitKeyword) == true) {
+                exit();
+                checkExitKeyword = true;
+                break;
+            } else {
+                response(userCommand);
+                userCommand = userInput.nextLine();
+            }
+        } while (checkExitKeyword == false);
+    }
+
+    public static void start() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -7,7 +30,6 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         greet();
-        exit();
     }
 
     public static void greet() {
@@ -17,5 +39,9 @@ public class Duke {
 
     public static void exit() {
         System.out.println("Bye. Hope to see you again soon!");
+    }
+
+    public static void response(String userCommand) {
+        System.out.println(userCommand);
     }
 }
